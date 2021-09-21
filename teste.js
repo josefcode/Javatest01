c144373553e81b51bcfa130413bc9cf1be6419c0
Atef Chelaghma <81826528+josefcode@users.noreply.github.com>
2021-09-21 15:14:34 -0700

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,20 +1,20 @@
-let numeros = [2, 4, 6, 8, 12]
+let numeros = [2, 4, 6, 8, 12];
 let impares = numeros.map(function(a){
-    return a - 1
-})
-console.log(impares)
-let numeroLista = [1, 3, 5, 6, 7]
+    return a - 1;
+});
+console.log(impares);
+let numeroLista = [1, 3, 5, 6, 7];
 let result = numeroLista.reduce(function(a, b){ 
-    return   a +" - " + b
-     })
-     console.log(result)
+    return   a +" - " + b;
+     });
+     console.log(result);
 
-     let nomes = ['atef', 'professor', 'linkon', 'Maria', 'Maria']
+     let nomes = ['atef', 'professor', 'linkon', 'Maria', 'Maria'];
 
-     let nomeEsp = nomes.filter(nome=> nome == "Maria")
+     let nomeEsp = nomes.filter(nome=> nome == "Maria");
     
-     console.log(nomeEsp)
+     console.log(nomeEsp);
  
-     let animais = ['vaca','cachorro','gato']
+     let animais = ['vaca','cachorro','gato'];
 
-     animais.forEach( a => console.log(`O animal é ${a}`))
+     animais.forEach( a => console.log(`O animal é ${a}`));
